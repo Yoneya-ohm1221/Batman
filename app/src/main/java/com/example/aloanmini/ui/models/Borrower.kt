be33f659c6c7b5b9007e352c96709909fa84b8c9
Jatuporn.sea <jatuporn.sea@turbo.co.th>
2022-06-14 17:49:17 +0700

    update model

--- app/src/main/java/com/example/aloanmini/ui/models/Borrower.kt
+++ app/src/main/java/com/example/aloanmini/ui/models/Borrower.kt
@@ -1,6 +1,10 @@
 package com.example.aloanmini.ui.models
 
 data class Borrower(
+    var key :String,
     var uid: String,
     var borrower_uid:String,
+    var amount:Float,
+    var start_date:String,
+    var end_date:String
 )
